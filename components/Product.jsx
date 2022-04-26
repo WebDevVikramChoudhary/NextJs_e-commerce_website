864553b4bf001423890bcdd9e4952153d6bf4ae4
Vikram Choudhary <90524740+WebDevVikramChoudhary@users.noreply.github.com>
2022-04-26 13:14:28 +0530

    Update $ to ₹

--- components/Product.jsx
+++ components/Product.jsx
@@ -15,7 +15,7 @@ const Product = ({ product: { image, name, slug, price } }) => {
             className="product-image"
           />
           <p className="product-name">{name}</p>
-          <p className="product-price">${price}</p>
+          <p className="product-price">₹{price}</p>
         </div>
       </Link>
     </div>
